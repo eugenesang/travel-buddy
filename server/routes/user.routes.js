2415ae6d8e5aca87288cb3a94d070c48cb5a8a22
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-03-31 21:37:33 +0300

    route user edit actions

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,10 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-import { registerUser, loginUser, updateUserProfile } from '../controllers/userController.js';
+import { registerUser, loginUser, updateUserProfile, updateName, updateAbout, updateLocation } from '../controllers/userController.js';
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.post('/name', updateName);
+router.post("/about", updateAbout);
+router.post("/location", updateLocation);
+
 router.put('/:id', updateUserProfile);
+
 
 export default router;
