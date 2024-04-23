8c1a0c7d1eee52b5c80274df197882dc8bfaf550
ChepchumbaSerem <chepchumbaserem@gmail.com>
2024-04-23 17:34:35 +0300

    Database name: Travel Connect

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/travel-connect';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017';
 
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            dbName: 'travel-buddy',
+            dbName: 'travel-connect',
         });
         console.log('Connected to MongoDB');
     } catch (err) {
