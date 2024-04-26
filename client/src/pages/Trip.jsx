ca87db1d736323e203f8da82574700894855f548
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-26 11:42:16 +0300

    update tourist attractions

--- client/src/pages/Trip.jsx
+++ client/src/pages/Trip.jsx
@@ -1,32 +1,50 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import { getUserTrips } from "../services/tripApi";
 
-import  { getUserTrips } from "../services/tripApi"
-import { Link } from 'react-router-dom';
+const Card = ({ trip }) => {
+    const { name, destination, tripDays, cost, image, location, totalDays, _id } =
+        trip;
+    const imgUrl = image
+        ? image
+        : `https://source.unsplash.com/random/800x600?tourist place of${location || destination
+        }`;
 
-// eslint-disable-next-line react/prop-types
-const TripCard = ({name, destination, cost, startDate, totalDays, _id}) =>{
     return (
-        <article className='trip-card' >
-            <h3>{name}</h3>
-            <h5><i className="fas fa-location-dot"></i> {destination}</h5>
-            <div className='other-details'>
-                <span className="info">
-                    <i className="fas fa-money-bill"></i> {cost}
-                </span>
-                <span className="info">
-                    <i className="fas fa-calender"></i> {new Date(startDate).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}
-                </span>
-                <span className='info'>
-                    {totalDays} days
-                </span>
+        <div className="home-trip-card">
+            <div className="img-container">
+                <img src={imgUrl} alt={name} />
+            </div>
+            <div className="content-container">
+                <div className="location">
+                    <i className="fas fa-location-dot"></i>
+                    <span className="destination-name">{destination || location}</span>
+                </div>
+                <h2 >{name}</h2>
+                <p>
+                    {tripDays || totalDays} days | Ksh {cost}
+                </p>
             </div>
             <div className='btn-container'>
-                <Link to={`/trips/${_id}`} className='button'>View</Link>
+                <a href={`/trips/${_id}`} className="button">view</a>
             </div>
-        </article>
-    )
-}
+        </div>
+    );
+};
+
+Card.propTypes = {
+    trip: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        destination: PropTypes.string,
+        tripDays: PropTypes.number,
+        cost: PropTypes.number.isRequired,
+        image: PropTypes.string,
+        location: PropTypes.string,
+        totalDays: PropTypes.number,
+        _id: PropTypes.string
+    }),
+};
 
 const Trip = () => {
 
@@ -39,34 +57,38 @@ const Trip = () => {
 
     useEffect(() => {
         const fetchTrips = async () => {
-          setLoading(true);
-    
-          try {
-            const trips = (await getUserTrips(user._id))
-            
-            setData(trips);
-            console.log(trips);
+            setLoading(true);
 
-          } catch (error) {
-            setError(true);
-            console.error("Error fetching trip details:", error);
-          } finally {
-            setLoading(false);
-          }
+            try {
+                const trips = (await getUserTrips(user._id))
+
+                setData(trips);
+                console.log(trips);
+
+            } catch (error) {
+                setError(true);
+                console.error("Error fetching trip details:", error);
+            } finally {
+                setLoading(false);
+            }
         };
-    
-        fetchTrips();
-      }, [user._id]);
 
-    return ( 
+        fetchTrips();
+    }, [user._id]);
+
+    return (
         <section id="trip" className='container'>
             {loading && <h3>Loading ...</h3>}
             {error && <h3>An error occurred, Try again later</h3>}
-            {data && data.map(({name, destination, cost, startDate, totalDays, _id})=>{
-                return <TripCard name={name} destination={destination} cost={cost} startDate={startDate} totalDays={totalDays} key={_id} _id={_id} />
-            })}
+            {data && (
+                <div className="trips-container">
+                    {data.map((trip) => {
+                        return <Card trip={trip} key={trip._id} />
+                    })}
+                </div>
+            )}
         </section>
-     );
+    );
 }
- 
+
 export default Trip;
