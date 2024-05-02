93859110786587664632e42c1d2c7c12e4181fdf
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-03 00:00:50 +0300

    destination route

--- server/routes/destination.routes.js
+++ server/routes/destination.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import  { getAll, create, createMany } from "../controllers/destination.controller.js";
+import  { getAll, create, createMany, explore, like } from "../controllers/destination.controller.js";
 
 const route = express.Router();
 
@@ -8,5 +8,8 @@ route.get('/', getAll);
 route.post("/", create);
 
 route.post('/many', createMany)
+
+route.get('/explore', explore)
+route.get('/like', like)
 
 export default route;
