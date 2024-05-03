5c50d78a28e4442eb8eaf0ebb5da834a9fafd662
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-03 05:53:28 +0300

    Explore page

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -3,17 +3,38 @@ import ExploreCard from "../components/ExploreCard";
 import useFetch from "../hooks/useFetch";
 
 const Explore = () => {
-    
-    const {error, data, loading} = useFetch("/api/destination/explore")
 
-    
-    return ( <section id="explore">
-        {loading &&  "Loading ..."}
-        {error && "An error occurred"}
-        {data && data.map((d, i)=>{
-            return (<ExploreCard key={i} title={d.title} location={d.location}  img={d.img} description={d.description} _id={d._id} />)
-        })}
-    </section> );
+    return (
+        <section id="explore">
+            <div className="inner-section">
+                <h1>Must visit destinations in <span>Kenya</span> <span className="tick">✔</span></h1>
+                <ExploreDestinations />
+            </div>
+            <div>
+                <h1>Visited Destinations</h1>
+            </div>
+        </section>
+    )
 }
- 
+
+function ExploreDestinations() {
+    const { error, data, loading } = useFetch("/api/destination/explore")
+
+    const cleanData = (arr) => {
+        const l = {};
+        for (let e of arr) {
+            l[e._id] = e;
+        }
+
+        return Object.values(l);
+    }
+    return (<div className="explore-destinations">
+        {loading && "Loading destinations..."}
+        {error && "An error occurred fetching destinations"}
+        {data && cleanData(data).map((d, i) => {
+            return (<ExploreCard key={i} title={d.title} location={d.location} img={d.img} description={d.description} _id={d._id} />)
+        })}
+    </div>);
+}
+
 export default Explore;
