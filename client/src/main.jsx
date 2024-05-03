d318c0d937b9845f3eb5f955fe2c47866e450c1c
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-03 05:55:09 +0300

    main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./assets/styles/index.scss";
@@ -8,13 +8,14 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store";
 
-ReactDOM.render(
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
